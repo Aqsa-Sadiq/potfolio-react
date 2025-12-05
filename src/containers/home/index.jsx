e7ddd4ddb418e4e945ef6e87ccadfa0a7c0d8f32
Aqsa-sadiq <aqsasadiq909@gmail.com>
2025-12-05 08:25:51 +0500

    last update

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -2,45 +2,48 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import { Animate } from 'react-simple-animate';
 import './styles.scss';
-import bg from "../../assets/back-bg.jpg";  
+import image from '../../assets/back-bg.jpg';
+
 
 const Home = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleNavigateToContactMe = () => {
-        navigate('/contact');
-    }
+  const handleNavigateToContactMe = () => {
+    navigate('/contact');
+  }
 
-    return (
-        <section 
-            id="home" 
-            className="home"
-            
-        >
-            <div className="home__text-wrapper">
-                <h1>
-                    Hello, I'm Aqsa
-                    <br/>
-                    Front End Developer
-                </h1>
-            </div>
+  return (
+    <section id="home" className="home" 
+    style={{
+  backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${image})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat'
+}}
+    >
+      <div className="home__text-wrapper">
+        <h1>
+          Hello, I'm Aqsa
+          <br />
+          Front End Developer
+        </h1>
+      </div>
 
-            <Animate
-                play
-                duration={1.5}
-                delay={1}
-                start={{ transform: 'translateY(550px)' }}
-                end={{ transform: 'translateX(0px)' }}
-            >
-                <div className="home__contact-me">
-                    <button onClick={handleNavigateToContactMe}>
-                        Hire Me
-                    </button>
-                </div>
-            </Animate>
-
-        </section>
-    );
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{ transform: 'translateY(550px)' }}
+        end={{ transform: 'translateX(0px)' }}
+      >
+        <div className="home__contact-me">
+          <button onClick={handleNavigateToContactMe}>
+            Hire Me
+          </button>
+        </div>
+      </Animate>
+    </section>
+  );
 }
 
 export default Home;
